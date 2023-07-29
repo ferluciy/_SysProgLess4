c07e3c81d9d4e75b8ecc37022a6d2fa9b06b08f7
Игорь <ferluc90@mail.ru>
2023-07-29 16:03:46 +0300

    edit1

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+using UnityEngine.Networking;
+[RequireComponent(typeof(CharacterController))]
+public abstract class Character : NetworkBehaviour
+{
+    protected Action OnUpdateAction { get; set; }
+    protected abstract FireAction fireAction { get; set; }
+    [SyncVar] protected Vector3 serverPosition;
+    [SyncVar] protected Quaternion serverRotation;
+    protected virtual void Initiate()
+    {
+        OnUpdateAction += Movement;
+    }
+    private void Update()
+    {
+        OnUpdate();
+    }
+    private void OnUpdate()
+    {
+        OnUpdateAction?.Invoke();
+    }
+    [Command]
+    protected void CmdUpdatePosition(Vector3 position, Quaternion rotation)
+    {
+        serverPosition = position;
+        serverRotation = rotation;
+    }
+    public abstract void Movement();
+}
